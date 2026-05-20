def4fdf138d6dd8943b673f192f267c6653977c9
Mark Tumlad <markfrancistumlad@gmail.com>
2026-05-20 14:27:04 +0800

    fix probably

--- api/test.php
+++ api/test.php
@@ -1,17 +1,5 @@
 <?php
+require 'config.php';
 header('Content-Type: application/json');
-
-$host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
-$db   = getenv('MYSQLDATABASE');
-$user = getenv('MYSQLUSER');
-$pass = getenv('MYSQLPASSWORD');
-
-echo json_encode([
-    'host'     => $host ?: 'EMPTY',
-    'port'     => $port ?: 'EMPTY',
-    'db'       => $db   ?: 'EMPTY',
-    'user'     => $user ?: 'EMPTY',
-    'pass_set' => $pass ? 'YES' : 'EMPTY'
-]);
+echo json_encode(['status' => 'connected']);
 ?>
